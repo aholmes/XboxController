95b2052824ce01eb3ee264a95a54920c47fbaa17
Aaron Holmes <aaron@aaronholmes.net>
2018-12-09 23:21:17 -0800

    Handle input events from xbox controller and pass correct memory address for raw input read.

--- XboxController/MainWindow.xaml.cs
+++ XboxController/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -20,12 +22,17 @@ namespace XboxController
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		private bool IsLoaded;
+
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
 
-			var intPtr = IntPtr.Zero;
-			var numberOfActiveDevices = RawInput.Net.RawInput.InitialiseGamepads(intPtr);
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			var mainWindowHandle = Process.GetCurrentProcess().MainWindowHandle;
+			var numberOfActiveDevices = RawInput.Net.RawInput.InitialiseGamepads(mainWindowHandle);
 
 			var devices = new IntPtr[numberOfActiveDevices];
 			for (var deviceIndex = 0; deviceIndex < numberOfActiveDevices; deviceIndex++)
@@ -33,17 +40,32 @@ namespace XboxController
 				devices[deviceIndex] = RawInput.Net.RawInput.GetDevicePath(deviceIndex);
 			}
 
-			while(true)
-			{
+			IsLoaded = true;
+		}
 
-				RawInput.Net.RawInput.ProcessInput(IntPtr.Zero, devices[0],
-					out byte buttons, out int x, out int y);
+		protected override void OnSourceInitialized(EventArgs e)
+		{
+			base.OnSourceInitialized(e);
+			var source = PresentationSource.FromVisual(this) as HwndSource;
+			source.AddHook(WndProc);
+		}
 
-				System.Diagnostics.Debug.Write(buttons);
-				System.Diagnostics.Debug.Write(x);
-				System.Diagnostics.Debug.WriteLine(y);
-				System.Threading.Thread.Sleep(100);
-			}
+		private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+		{
+			if (!IsLoaded || msg != 0x00ff) return IntPtr.Zero;
+			// Handle messages...
+
+			RawInput.Net.RawInput.ProcessInput(wParam, lParam,
+				out byte buttons, out int x, out int y);
+
+			System.Diagnostics.Debug.Write(buttons + "|");
+			System.Diagnostics.Debug.Write(x+"|");
+			System.Diagnostics.Debug.WriteLine(y);
+
+
+			handled = true;
+
+			return IntPtr.Zero;
 		}
 	}
 }
